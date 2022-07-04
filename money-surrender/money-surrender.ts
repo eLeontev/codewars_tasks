3c74b827ffe5f37105dbe547e8a848d40099f48b
eLeontev <joonior199@gmail.com>
2022-07-04 12:17:43 +0200

    #1 money-surrender: add task description

--- money-surrender/money-surrender.ts
+++ money-surrender/money-surrender.ts
@@ -6,5 +6,12 @@ export const moneySurrender = <I extends currencies, O extends currencies>(
     currencyA: Currency<I>,
     currencyB: Currency<O>
 ): Currency<O> => {
+    /**
+     * let's imagine you are gonna to buy smt by cash in currency A, but surrender is possible only in cash B
+     * so your code should be able to perform that transition based on provided tests
+     *
+     * p.s. all types and constants already implemented, please use them
+     */
+
     return; // TODO: your magic is here
 };
